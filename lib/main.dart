0b436a63b67061655ca5a32a9c3e264a052f17f4
cayaa <162942854+Cahyanandita@users.noreply.github.com>
2024-08-07 18:59:27 +0700

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,365 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Football Match App',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MatchList(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class MatchList extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MatchListState createState() => _MatchListState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MatchListState extends State<MatchList> {
+  int _selectedIndex = 0;
 
-  void _incrementCounter() {
+  void _onItemTapped(int index) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _selectedIndex = index;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      backgroundColor: const Color.fromARGB(250, 250, 250, 250),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 40), // For status bar padding
+            Text('Glad to see you,', style: TextStyle(fontSize: 16)),
+            Text('Esther Howard!',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+            SizedBox(height: 20),
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Find your favorite club',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                prefixIcon: Icon(Icons.search),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+
+            SizedBox(height: 20),
+            Text('Live Match',
+                style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold)),
+            SizedBox(height: 10),
+            Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(1),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text('Premier League', style: TextStyle(fontSize: 16)),
+                        Text("78'",
+                            style: TextStyle(fontSize: 16, color: Colors.red)),
+                      ],
+                    ),
+                    SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset('Arsenal_FC..png', height: 60),
+                        SizedBox(width: 20),
+                        RichText(
+                          text: TextSpan(
+                            style: TextStyle(
+                                fontSize: 26,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black),
+                            children: [
+                              TextSpan(
+                                  text: '2',
+                                  style: TextStyle(
+                                      color: const Color.fromARGB(
+                                          255, 61, 182, 135))),
+                              TextSpan(text: ' : '),
+                              TextSpan(text: '1'),
+                            ],
+                          ),
+                        ),
+                        SizedBox(width: 20),
+                        Image.asset('Chelsea_FC.png', height: 60),
+                      ],
+                    ),
+                    SizedBox(height: 10),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => LiveScoreScreen()),
+                        );
+                      },
+                      child: Text('Live'),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color.fromARGB(255, 248, 248, 248),
+                        side: BorderSide(
+                          color: const Color.fromARGB(
+                              255, 61, 182, 135), // Set green border color
+                          width: 2.0, // Adjust border width (optional)
+                        ),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        foregroundColor: Color.fromARGB(255, 61, 182, 135),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('Finished Match',
+                    style:
+                        TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                Icon(Icons.arrow_forward),
+              ],
+            ),
+            SizedBox(height: 10),
+            Expanded(
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    FinishedMatchCard(
+                      league: 'Champions League',
+                      team1Logo: 'Juventus_FC.png',
+                      team2Logo: 'Paris_Saint-.png',
+                      score: '1 : 0',
+                    ),
+                    SizedBox(width: 10),
+                    FinishedMatchCard(
+                      league: 'League B',
+                      team1Logo: 'Bayern_FC.png',
+                      team2Logo: 'Borussia_.png',
+                      score: '0 : 2',
+                    ),
+                  ],
+                ),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home_outlined), label: ''),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.notifications_outlined), label: ''),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person_2_outlined), label: ''),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: const Color.fromARGB(255, 61, 182, 135),
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
+
+class FinishedMatchCard extends StatelessWidget {
+  final String league;
+  final String team1Logo;
+  final String team2Logo;
+  final String score;
+
+  const FinishedMatchCard({
+    Key? key,
+    required this.league,
+    required this.team1Logo,
+    required this.team2Logo,
+    required this.score,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    List<String> scores = score.split(' : ');
+    int team1Score = int.parse(scores[0]);
+    int team2Score = int.parse(scores[1]);
+
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(league, style: TextStyle(fontSize: 16)),
+              ],
+            ),
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(team1Logo, height: 50),
+                SizedBox(
+                  width: 30,
+                  height: 30,
+                ),
+                RichText(
+                  text: TextSpan(
+                    style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black),
+                    children: [
+                      TextSpan(
+                          text: scores[0],
+                          style: TextStyle(
+                              color: team1Score > team2Score
+                                  ? const Color.fromARGB(255, 61, 182, 135)
+                                  : Colors.black)),
+                      TextSpan(text: ' : '),
+                      TextSpan(
+                          text: scores[1],
+                          style: TextStyle(
+                              color: team2Score > team1Score
+                                  ? const Color.fromARGB(255, 61, 182, 135)
+                                  : Colors.black)),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  width: 50,
+                  height: 30,
+                ),
+                Image.asset(team2Logo, height: 50),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class LiveScoreScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        title: Text('Champions League'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Yesterday',
+              style: TextStyle(fontSize: 16, color: Colors.grey),
+            ),
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  children: [
+                    Image.asset(
+                      'Juventus_FC.png',
+                      width: 60,
+                      height: 60,
+                    ),
+                    Text('Juventus'),
+                  ],
+                ),
+                RichText(
+                  text: TextSpan(
+                    style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black),
+                    children: [
+                      TextSpan(
+                          text: '2', style: TextStyle(color: Colors.green)),
+                      TextSpan(text: ' : '),
+                      TextSpan(text: '1'),
+                    ],
+                  ),
+                ),
+                Column(
+                  children: [
+                    Image.asset(
+                      'Paris_Saint-.png',
+                      width: 60,
+                      height: 60,
+                    ),
+                    Text('Paris S-G'),
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(height: 30),
+            ListTile(
+              leading: CircleAvatar(
+                backgroundImage: AssetImage(
+                  'images.jpeg',
+                ),
+              ),
+              title: Text('Top Player'),
+              subtitle: Text('Paulo Dybala'),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Match Stats',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+            Expanded(
+              child: ListView(
+                children: [
+                  ListTile(
+                    title: Text('Shots'),
+                    trailing: Text('14'),
+                  ),
+                  ListTile(
+                    title: Text('Shots on target'),
+                    trailing: Text('2'),
+                  ),
+                  ListTile(
+                    title: Text('Possession'),
+                    trailing: Text('36'),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
